936176ccdf0cd57ed52e81480e58433e11160b9d
haoyuan <chytom01@gmail.com>
2025-05-06 23:19:53 +0800

    Update TrailerDecoder.java

--- fast-fix-parser/src/main/java/com/fix/decoder/TrailerDecoder.java
+++ fast-fix-parser/src/main/java/com/fix/decoder/TrailerDecoder.java
@@ -7,8 +7,10 @@ import static com.fix.constant.Constants.*;
 import static com.fix.util.bytes.BytesScanner.*;
 
 /**
- * Only include the mandatory fields of the message trailer in this example.
- * The non-mandatory fields should follow the same paradigm and created automatically by a code-gen template.
+ *
+ * Only include the mandatory fields of the message header in this example.
+ * The non-mandatory fields MUST be added if used in production.
+ * And should be added following the same paradigm and created automatically by a code-gen template.
  *
  * @author Tom Haoyuan Chen
  */
